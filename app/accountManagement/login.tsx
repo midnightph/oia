4715f906bcbcfe207bda5c646d979006b31c990b
RichasTheEgg <sdlakira2@gmail.com>
2025-11-18 20:47:11 -0300

    feat: Implement login screen UI with animations
    Develop the user interface for the login page, including email and password input fields. Add a password visibility toggle and a login button.
    Integrate the 'moti' library for entrance animations on the login form.
    Introduce a new 'components/colors.tsx' file to centralize color definitions.

--- app/accountManagement/login.tsx
+++ app/accountManagement/login.tsx
@@ -1,9 +1,68 @@
-import { Text, View } from "react-native";
+import colors from '@/components/colors';
+import { Feather } from '@expo/vector-icons';
+import { MotiView } from 'moti';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-export default function Login(){
-    return(
-        <View>
-            <Text>login</Text>
-        </View>
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [secure, setSecure] = useState(false)
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            <MotiView
+                from={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ type: 'timing', duration: 1000 }}>
+                <Text style={styles.title}>Entre na sua conta</Text>
+                <TextInput placeholder="Email" style={styles.input} />
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: 10 }}>
+                    <TextInput placeholder="Senha" style={[styles.input, { width: '70%' }]} secureTextEntry={secure} />
+                    <TouchableOpacity onPress={() =>
+                        setSecure(!secure)} style={styles.eyeButton}>
+                        <Feather name={secure ? 'eye' : 'eye-off'} size={20} color={colors.title} />
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity style={{
+                    height: 45,
+                    width: '80%',
+                    borderWidth: 1,
+                    borderColor: colors.title,
+                    borderRadius: 16,
+                    paddingHorizontal: 16,
+                    alignSelf: 'center', 
+                    justifyContent: 'center',
+                    backgroundColor: colors.light
+                }}>
+                    <Text style={{ alignSelf: 'center', color: colors.title, fontSize: 16, fontWeight: '600' }}>Entrar</Text>
+                </TouchableOpacity>
+            </MotiView >
+        </View >
     )
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: colors.title,
+        paddingBottom: 16,
+        alignSelf: 'center'
+    },
+    input: {
+        height: 45,
+        width: '80%',
+        borderWidth: 1,
+        borderColor: colors.title,
+        borderRadius: 16,
+        paddingHorizontal: 16,
+        marginBottom: 16,
+        alignSelf: 'center'
+    },
+    eyeButton: {
+        paddingHorizontal: 6,
+        marginBottom: 16,
+    },
+});
---
+++ components/colors.tsx
@@ -0,0 +1,6 @@
+const colors = {
+    title: '#1D3D47',
+    light: '#A1CEDC'
+}
+
+export default colors
